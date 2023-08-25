0380d920f8604fc85f3ae31798f6c558a6429043
luthfi2603 <luthfim904@gmail.com>
2023-08-25 20:37:06 +0700

    merubah susunan repositori

---
+++ himatif_konseling.js
@@ -0,0 +1,30 @@
+// number
+// document.writeln(100);
+// document.writeln("<br>");
+// document.writeln(10.5);
+// document.writeln("<br>");
+
+// // di dalam petik 2 dapat petik 1
+// document.writeln("Hari ini hari jum'at")
+
+// var itu untuk global scope
+// let hanya berlaku untuk suatu fungsi saja, tidak untuk semua
+// == hanya melihat nilai nya
+// === melihat semua tipe data
+// ! menampilkan kebalikannya
+// && (dan) true true => true, true false => false, kedua benar baru benar
+// || (atau) hanya salah satu dapat menjadi true
+
+
+/*var hobi = [game, anime, music];
+    for(i in hobi){
+        document.writeln("Hobi saya adalah : " +hobi[i]+ "<br>");
+    }*/
+// for(let a = 1; a <= 10; a++){
+//     document.writeln(a+ "<br>")
+// }
+var hewan = ['kucing', 'kuda', 'kodok', 'harimau'];
+    for(let x in hewan){
+        document.write("hewan : " +hewan[x]+ "<br>")
+    }
+    // document.getElementById("hadeh").innerHTML = test;
